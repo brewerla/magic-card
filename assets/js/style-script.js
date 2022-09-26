ecfb05e933f72f86d1cf4690522b7ab84584e198
brewerla <brewerla02@gmail.com>
2022-09-26 12:52:34 -0500

    add previous card action

--- assets/js/style-script.js
+++ assets/js/style-script.js
@@ -8,11 +8,22 @@ for(let i = 1; i < children.length; i++) {
 let currentChildIndex = 0;
 
 let nextElement = () => {
+    if(!children[currentChildIndex + 1]) return;
     currentChildIndex++;
     children[currentChildIndex - 1].classList.add("magic-card_flipped_left");
     children[currentChildIndex].classList.remove("magic-card_flipped_right");
 }
 
-Array.from(document.getElementsByClassName("magic-card_button_box")).forEach(button => {
-    button.addEventListener('click', nextElement);
-})
+let prevElement = () => {
+    if(!children[currentChildIndex - 1]) return;
+    currentChildIndex--;
+    children[currentChildIndex + 1].classList.add("magic-card_flipped_right");
+    children[currentChildIndex].classList.remove("magic-card_flipped_left");
+}
+let buttonBoxes = Array.from(document.getElementsByClassName("magic-card_button_box"));
+
+for(let buttonBox of buttonBoxes) {
+    Array.from(buttonBox.children).forEach(child => {
+        child.addEventListener('click', (Array.from(child.classList)[0] === "next") ? nextElement : prevElement);
+    });
+}
